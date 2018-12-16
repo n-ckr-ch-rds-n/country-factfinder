3d3c77dfc63b56d48e82c10e1f05b4b484cfda88
Nick <nick.richardson@rezonence.com>
2018-12-16 16:04:00 +0000

    Style fact-window

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import {GraphQLModule} from './apollo.config';
 import { CountryComponent } from './country/country.component';
 import { CountryListComponent } from './country-list/country-list.component';
 import { FactWindowComponent } from './fact-window/fact-window.component';
-import {MatListModule} from '@angular/material';
+import {MatCardModule, MatListModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {NgxPaginationModule} from 'ngx-pagination';
@@ -23,6 +23,7 @@ import {NgxPaginationModule} from 'ngx-pagination';
     GraphQLModule,
     BrowserAnimationsModule,
     MatListModule,
+    MatCardModule,
     FlexLayoutModule,
     NgxPaginationModule
   ],
